0a8fc943cbaf5007036b1be8727d2795dcad4876
gaws <it20051402@my.sliit.lk>
2023-10-10 08:17:26 +0530

    reservation part

--- src/Pages/ReservationManagement/index.js
+++ src/Pages/ReservationManagement/index.js
@@ -1,68 +1,45 @@
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { Box, DialogActions, Button, DialogContent, DialogContentText, DialogTitle, Dialog, Radio, FormControlLabel, RadioGroup, FormLabel, FormControl } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import DescriptionAlerts from '../../Components/AlertMsg/Alert';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
-
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
+import DescriptionAlerts from '../../Components/AlertMsg/Alert';
 
 const ReservationManagement = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [open, setOpen] = useState(false);
-  const [newType, setNewType] = useState('')
-  const [deleteId, setDeleteId] = useState('')
-  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false)
+  const [newStatus, setNewStatus] = useState('');
+  const [deleteId, setDeleteId] = useState('');
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
   const [openSnackbar, setOpenSnackbar] = useState(false);
-  const [severity, setSeverity] = useState('')
-  const [alertMessage, setAlertMessage] = useState('')
-  const [selectedUser, setSelectedUser] = useState([])
-  const [users, setUsers] = useState([])
+  const [severity, setSeverity] = useState('');
+  const [alertMessage, setAlertMessage] = useState('');
+  const [selectedReservation, setSelectedReservation] = useState({});
+  const [reservations, setReservations] = useState([]);
 
   useEffect(() => {
-    axios.get(`https://localhost:7064/api/user`)
+    // Fetch reservations data from the API when the component mounts
+    axios.get('/reservation')
       .then((res) => {
-        setUsers(res.data)
-
+        setReservations(res.data);
       })
-  }, [])
+      .catch((err) => {
+        console.error(err);
+        if (err.response.status === 401) {
+          navigate('/');
+        }
+      });
+  }, []);
 
   const handleCloseSnackbar = () => {
     setOpenSnackbar(false);
   };
 
-  const editButtonClicked = (user) => {
-    setSelectedUser(user)
-    handleClickOpen()
-  }
+  const editButtonClicked = (reservation) => {
+    setSelectedReservation(reservation);
+    handleClickOpen();
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -70,97 +47,74 @@ const ReservationManagement = () => {
 
   const handleClose = () => {
     setOpen(false);
-    setNewType('')
-    setSelectedUser([])
+    setNewStatus('');
+    setSelectedReservation({});
   };
 
   const handleOpenDelete = () => {
-    setDeleteDialogOpen(true)
-  }
+    setDeleteDialogOpen(true);
+  };
 
   const handleCloseDelete = () => {
-    setDeleteDialogOpen(false)
-    setDeleteId('')
-  }
+    setDeleteDialogOpen(false);
+    setDeleteId('');
+  };
 
   const deleteButtonClicked = (id) => {
-    setDeleteId(id)
-  }
+    setDeleteId(id);
+  };
 
   useEffect(() => {
     if (deleteId) {
-      handleOpenDelete()
+      handleOpenDelete();
     }
-  }, [deleteId])
+  }, [deleteId]);
 
-  const handleType = (e) => {
-    setNewType(e.target.value)
-  }
+  const handleStatusChange = (e) => {
+    setNewStatus(e.target.value);
+  };
 
   const handleUpdate = () => {
-    axios.put(`https://localhost:7064/api/user/${selectedUser.id}`, {
-      id:selectedUser.id,
-      name:selectedUser.name,
-      password: selectedUser.password,
-      email: selectedUser.email,
-      nic: selectedUser.nic,
-      phone: selectedUser.phone,
-      userType: newType,
-      isActive:selectedUser.isActive
+    axios.put(`/reservation/${selectedReservation.id}`, {
+      ...selectedReservation,
+      status: newStatus,
     })
       .then((response) => {
-        console.log(response)
-        setNewType('')
-        setSelectedUser([])
-        handleClose()
-        setSeverity('success')
-        setAlertMessage('User Type has been changed successfully')
+        console.log(response);
+        setNewStatus('');
+        setSelectedReservation({});
+        handleClose();
+        setSeverity('success');
+        setAlertMessage('Reservation status has been updated successfully');
         setOpenSnackbar(true);
-      }).catch((err) => {
-        console.log(err)
-        if (err.response.status === 401) {
-          navigate('/')
-        }
       })
-  }
+      .catch((err) => {
+        console.error(err);
+        if (err.response.status === 401) {
+          navigate('/');
+        }
+      });
+  };
 
   const handleDelete = () => {
-    axios.delete(`https://localhost:7064/api/user/${deleteId}`)
+    axios.delete(`/reservation/${deleteId}`)
       .then((response) => {
-        setDeleteId(false)
-        handleCloseDelete()
-        setSeverity('success')
-        setAlertMessage('Deleted sucessfully')
+        setDeleteId('');
+        handleCloseDelete();
+        setSeverity('success');
+        setAlertMessage('Reservation deleted successfully');
         setOpenSnackbar(true);
-      }).catch((err) => {
-        console.log(err)
-        setSeverity('error')
-        setAlertMessage(err.response.data.message)
+      })
+      .catch((err) => {
+        console.error(err);
+        setSeverity('error');
+        setAlertMessage(err.response.data.message);
         setOpenSnackbar(true);
         if (err.response.status === 401) {
-          navigate('/')
+          navigate('/');
         }
-      })
-  }
-
-  const editUserStatus = (user) => {
-    user.isActive = !user.isActive
-    console.log(user)
-    axios.put(`https://localhost:7064/api/user/${user.id}`,{
-      id:user.id,
-      name:user.name,
-      password: user.password,
-      email: user.email,
-      nic: user.nic,
-      phone: user.phone,
-      userType: user.userType,
-      isActive:user.isActive
-    }).then((res)=>{
-      setSeverity('success')
-      setAlertMessage(`Account has been ${user.isActive ? "activated" : "deacivated"} successfully`)
-      setOpenSnackbar(true);
-    })
-  }
+      });
+  };
 
   return (
     <Box>
@@ -170,154 +124,91 @@ const ReservationManagement = () => {
           <Table sx={{ minWidth: 700 }} aria-label="customized table">
             <TableHead>
               <TableRow>
-                <StyledTableCell>Name</StyledTableCell>
-                <StyledTableCell align="left">Email</StyledTableCell>
-                <StyledTableCell align="left">NIC</StyledTableCell>
-                <StyledTableCell align="left">Phone Number</StyledTableCell>
-                <StyledTableCell align="left">UserType</StyledTableCell>
-                <StyledTableCell align="left">Status</StyledTableCell>
-                <StyledTableCell align="letf">Delete</StyledTableCell>
+                <TableCell align="left">UserId</TableCell>
+                <TableCell align="left">TrainID</TableCell>
+                <TableCell align="left">reservationDate</TableCell>
+                <TableCell align="left">Reservation Status</TableCell>
+                <TableCell align="left">Actions</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {users.map((user) => (
-                <StyledTableRow key={user.id} sx={{
-                  ":hover": { backgroundColor: '#666' }, "&.custom-row": {
-                    backgroundColor: user.isActive ? '#90EE90' : '#FF5733',
-                  },
-                }}
-                  className="custom-row">
-                  <StyledTableCell component="th" scope="row">
-                    {user.name}
-                  </StyledTableCell>
-                  <StyledTableCell component="th" scope="row">
-                    {user.email}
-                  </StyledTableCell>
-                  <StyledTableCell component="th" scope="row">
-                    {user.nic}
-                  </StyledTableCell>
-                  <StyledTableCell component="th" scope="row">
-                    {user.phone}
-                  </StyledTableCell>
-                  <StyledTableCell component="th" scope="row">
-                    <Button sx={{ color: 'black', textTransform: 'capitalize', ':hover': { backgroundColor: 'black', color: 'white' } }}
-                      onClick={() => editButtonClicked(user)}>
-                      {user.userType}
+              {reservations.map((reservation) => (
+                <TableRow key={reservation.id}>
+                  <TableCell component="th" scope="row">{reservation.userID}</TableCell>
+                  <TableCell>{reservation.trainID}</TableCell>
+                  <TableCell>{reservation.reservationDate}</TableCell>
+                  <TableCell>{reservation.status}</TableCell>
+                  <TableCell>{reservation.status}</TableCell>
+                  <TableCell>
+                    <Button
+                      sx={{ color: 'black', textTransform: 'capitalize' }}
+                      onClick={() => editButtonClicked(reservation)}
+                    >
+                      {reservation.status ? 'Active' : 'Inactive'}
                     </Button>
-                  </StyledTableCell>
-                  <StyledTableCell component="th" scope="row">
-                    <Button sx={{ color: 'black', textTransform: 'capitalize', ':hover': { backgroundColor: 'black', color: 'white' } }}
-                      onClick={() => editUserStatus(user)}>
-                      <ChangeCircleIcon/>
+                  </TableCell>
+                  <TableCell>
+                    <Button
+                      sx={{ color: 'black', textTransform: 'capitalize' }}
+                      onClick={() => handleUpdate()}
+                    >
+                      <ChangeCircleIcon />
                     </Button>
-                  </StyledTableCell>
-                  <StyledTableCell component="th" scope="row"><DeleteIcon sx={{
-                    borderRadius: '5px',
-                    width: '40px',
-                    height: '30px',
-                    ":hover": {
-                      backgroundColor: 'black',
-                      color: 'white'
-                    }
-                  }} onClick={() => deleteButtonClicked(user.id)} /></StyledTableCell>
-                </StyledTableRow>
+                    <Button
+                      sx={{
+                        borderRadius: '5px',
+                        width: '40px',
+                        height: '30px',
+                      }}
+                      onClick={() => deleteButtonClicked(reservation.id)}
+                    >
+                      <DeleteIcon />
+                    </Button>
+                  </TableCell>
+                </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
       </Box>
 
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Update Reservation Status</DialogTitle>
+        <DialogContent>
+          <FormControl>
+            <FormLabel>Status</FormLabel>
+            <RadioGroup
+              onChange={handleStatusChange}
+              value={newStatus}
+            >
+              <FormControlLabel value='active' control={<Radio />} label='Active' />
+              <FormControlLabel value='inactive' control={<Radio />} label='Inactive' />
+            </RadioGroup>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleUpdate} color="primary">Update Status</Button>
+        </DialogActions>
+      </Dialog>
 
-      <Box>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle sx={{ backgroundColor: 'black', color: 'white', textAlign: 'center' }}>Update UserType</DialogTitle>
-          <DialogContent sx={{ mt: '10px' }}>
-            <FormControl sx={{ mt: '10px' }}>
-              <FormLabel id="radio-buttons-group-label">Types</FormLabel>
-              <RadioGroup
-                onChange={handleType}
-                defaultValue={selectedUser.status}
-              >
-                <FormControlLabel value={'traveler'} control={<Radio />} label='Traveler' />
-                <FormControlLabel value={'user'} control={<Radio />} label='User' />
-                <FormControlLabel value={'traveler agent'} control={<Radio />} label='Traveler agent' />
-                <FormControlLabel value={'backofficer'} control={<Radio />} label='Backofficer' />
-              </RadioGroup>
-            </FormControl>
-          </DialogContent>
-          <DialogActions>
-            <Button
-              sx={{
-                backgroundColor: 'black',
-                width: '30%',
-                color: 'white',
-                ":hover": {
-                  backgroundColor: 'red',
-                  color: 'white',
-                  cursor: 'pointer'
-                },
-              }} onClick={handleClose}>Cancel</Button>
-            <Button
-              sx={{
-                backgroundColor: 'black',
-                color: 'white',
-                width: '70%',
-                ":hover": {
-                  backgroundColor: '#50C878',
-                  color: 'white',
-                  cursor: 'pointer'
-                },
-              }}
-              onClick={handleUpdate}
-            >Update Type</Button>
-          </DialogActions>
-        </Dialog>
-      </Box>
-
-
-
-      <Box>
-        <Dialog open={deleteDialogOpen} onClose={handleCloseDelete}>
-          <DialogContent sx={{ mt: '10px', }}>
-            <DialogContentText >
-              <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-
-              </Box>
-              Are you sure!
-              Do you want to delete it?
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button
-              sx={{
-                backgroundColor: 'black',
-                width: '50%',
-                color: 'white',
-                ":hover": {
-                  backgroundColor: 'red',
-                  color: 'white',
-                  cursor: 'pointer'
-                },
-              }} onClick={handleCloseDelete}>No! Don't</Button>
-            <Button
-              sx={{
-                backgroundColor: 'black',
-                color: 'white',
-                width: '50%',
-                ":hover": {
-                  backgroundColor: '#50C878',
-                  color: 'white',
-                  cursor: 'pointer'
-                },
-              }}
-              onClick={handleDelete}
-            >yes, please</Button>
-          </DialogActions>
-        </Dialog>
-      </Box>
+      <Dialog open={deleteDialogOpen} onClose={handleCloseDelete}>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to delete this reservation?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDelete} color="primary">
+            No
+          </Button>
+          <Button onClick={handleDelete} color="primary">
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 }
 
-
-export default ReservationManagement
+export default ReservationManagement;
